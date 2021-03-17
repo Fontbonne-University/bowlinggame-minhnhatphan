5cd582e4de087c35963a46be654ebc66f56f50b1
Minh Phan <nminh238@gmail.com>
2021-03-17 11:17:05 +0700

    message

--- BowlingGame/Bowling/src/BowlingGame/BowlingTest.java
+++ BowlingGame/Bowling/src/BowlingGame/BowlingTest.java
@@ -7,15 +7,67 @@ import org.junit.Ignore;
 import org.junit.Test;
 
 public class BowlingTest {
-
+	
+	private Game g;
+	
 	@Before
 	public void setUp() throws Exception {
-
+		g = new Game();
+	}
+	
+	private void rollStrike() {
+		g.roll(10);
+	}
+	
+	private void rollMany(int j, int pins) {
+		for (int i=0; i<j; i++) {
+			g.roll(pins);
+		}
 	}
 
 	@Test
-	public void canCreateGame()
+	public void canRoll() 
 	{
-		Game g = new Game();
+		g.roll(0);
+	}
+	
+	@Test
+	public void gutterGame()
+	{
+		rollMany(20, 0);
+		assertEquals(0, g.score());
+	}
+
+	@Test
+	public void allones()
+	{
+		rollMany(20, 1);
+		assertEquals(20, g.score());
+	}
+	
+	@Test
+	public void oneSpare()
+	{
+		g.roll(5);
+		g.roll(5); //spare
+		g.roll(3);
+		rollMany(17, 0);
+		assertEquals(16, g.score());
+	}
+	
+	@Test
+	public void oneStrike() 
+	{
+		rollStrike();
+		g.roll(3);
+		g.roll(4);
+		rollMany(0, 16);
+		assertEquals(24, g.score());
+	}
+
+	@Test
+	public void perfectGame() {
+		rollMany(12, 10);
+		assertEquals(300, g.score());
 	}
 }
--- BowlingGame/Bowling/src/BowlingGame/Game.java
+++ BowlingGame/Bowling/src/BowlingGame/Game.java
@@ -1,6 +1,39 @@
 package BowlingGame;
 
 public class Game {
+	
+	private int[] rolls = new int[21];
+	private int currentRoll = 0;
+	
+	public void roll(int pins) {
+		rolls[currentRoll++] = pins;
+	}
+
+	public int score() {
+		int score = 0;
+		int firstInFrame=0;
+		for (int frame=0; frame<10;frame++) {
+			if (rolls[firstInFrame]==10) {
+				score += 10 + rolls[firstInFrame+1]+ rolls[firstInFrame+2];
+				firstInFrame +=1;
+			}
+			else if (isSpare(firstInFrame)) {
+				score+=10 + rolls[firstInFrame+2];
+				firstInFrame+=2;
+			}
+			else {
+				score+=rolls[firstInFrame] + rolls[firstInFrame+1];
+				firstInFrame+=2;
+			}
+			
+		}
+		
+		return score;
+	}
+
+	private boolean isSpare(int firstInFrame) {
+		return rolls[firstInFrame]+rolls[firstInFrame+1]==10;
+	}
 
 
 }
